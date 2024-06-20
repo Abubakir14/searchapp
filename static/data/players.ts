28690d0c24ea7a5e96b0f01849010f2b0b73f564
Abubakir <uzbekovabubakir@gmail.com>
2024-06-20 18:47:12 +0600

    download dependecies and create data players, webpack

---
+++ static/data/players.ts
@@ -0,0 +1,5 @@
+export const players = [
+    { id: 1, name: 'Player One', profile: 'Profile of Player One' },
+    { id: 2, name: 'Player Two', profile: 'Profile of Player Two' },
+    // добавьте еще игроков по необходимости
+];
